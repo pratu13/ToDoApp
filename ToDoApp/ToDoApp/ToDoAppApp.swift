dc2a988a5ae4577725e58f440a453dfe56aa38a1
pratu13 <41483788+pratu13@users.noreply.github.com>
2021-10-07 18:05:15 -0400

    Add files via upload

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Pratyush  on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
